a318edf33ee4f1163df0ca87fc7142e331ce2783
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2018-03-15 19:34:28 -0300

    uniao entre duas listas

--- uniao.hs
+++ uniao.hs
@@ -4,11 +4,5 @@ main = do
 uniao :: [Int] -> [Int] -> [Int]
 uniao l1 [] = l1
 uniao [] l2 = l2
-uniao (a:as) l2 | contem a l2 = [] ++ uniao as l2
+uniao (a:as) l2 | a `elem` l2 = [] ++ uniao as l2
 	| otherwise = (a:[]) ++ uniao as l2
-
-
-contem :: Int -> [Int] -> Bool
-contem x [] = False
-contem x (a:as) | (a == x) = True
-	|otherwise = contem x as
